27cec80b71e245de808beee81dbda1b64b7c87d5
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-07-26 16:20:41 +1200

    add bGrid componenet, add background image

---
+++ src/Grid.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Grid extends React.Component {
+  render() {
+    return <div>Grid</div>;
+  }
+}
+
+export default Grid;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Grid from "./Grid.js";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = { generation: 0 };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Game of Life</h1>
+        <Grid />
+        <h2>Generations: {this.state.generation}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Main />, document.getElementById("root"));
