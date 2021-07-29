31762bf2100f22fb3cc4cbbcee026c1f6fedf91f
Nick Wittenberg <nickwittenberg@developersinstitute.co.nz>
2021-07-29 13:15:31 +1200

    refactor Cell.js and Grid.js from classes to functions git push

--- src/Cell.js
+++ src/Cell.js
@@ -1,16 +1,34 @@
 import React from "react";
-class Cell extends React.Component {
-  selectCell = () => {
-    this.props.selectCell(this.props.row, this.props.col);
+
+// CLASS SYNTAX
+
+// class Cell extends React.Component {
+//   selectCell = () => {
+//     this.props.selectCell(this.props.row, this.props.col);
+//   };
+//   render() {
+//     return (
+//       <div
+//         className={this.props.cellClass}
+//         id={this.props.cellId}
+//         onClick={this.selectCell}
+//       ></div>
+//     );
+//   }
+// }
+// export default Cell;
+
+const Cell = (props) => {
+  const selectCell = () => {
+    props.selectCell(props.row, props.col);
   };
-  render() {
-    return (
-      <div
-        className={this.props.cellClass}
-        id={this.props.cellId}
-        onClick={this.selectCell}
-      ></div>
-    );
-  }
-}
+
+  return (
+    <div
+      className={props.cellClass}
+      id={props.cellId}
+      onClick={selectCell}
+    ></div>
+  );
+};
 export default Cell;
--- src/Grid.js
+++ src/Grid.js
@@ -1,33 +1,62 @@
 import React from "react";
 import Cell from "./Cell";
 
-class Grid extends React.Component {
-  render() {
-    const width = this.props.cols * 16;
-    let rowsArr = [];
-    let cellClass = "";
-    for (let i = 0; i < this.props.rows; i++) {
-      for (let j = 0; j < this.props.cols; j++) {
-        let cellId = i + "_" + j;
-        cellClass = this.props.gridFull[i][j] ? "cell live" : "cell dead";
-        rowsArr.push(
-          <Cell
-            cellClass={cellClass}
-            key={cellId}
-            cellId={cellId}
-            row={i}
-            col={j}
-            selectCell={this.props.selectCell}
-          />
-        );
-      }
+// CLASS SYNTAX
+
+// class Grid extends React.Component {
+//   render() {
+//     const width = this.props.cols * 16;
+//     let rowsArr = [];
+//     let cellClass = "";
+//     for (let i = 0; i < this.props.rows; i++) {
+//       for (let j = 0; j < this.props.cols; j++) {
+//         let cellId = i + "_" + j;
+//         cellClass = this.props.gridFull[i][j] ? "cell live" : "cell dead";
+//         rowsArr.push(
+//           <Cell
+//             cellClass={cellClass}
+//             key={cellId}
+//             cellId={cellId}
+//             row={i}
+//             col={j}
+//             selectCell={this.props.selectCell}
+//           />
+//         );
+//       }
+//     }
+//     return (
+//       <div className="grid" style={{ width: width }}>
+//         {rowsArr}
+//       </div>
+//     );
+//   }
+// }
+
+const Grid = (props) => {
+  const width = props.cols * 16;
+  let rowsArr = [];
+  let cellClass = "";
+  for (let i = 0; i < props.rows; i++) {
+    for (let j = 0; j < props.cols; j++) {
+      let cellId = i + "_" + j;
+      cellClass = props.gridFull[i][j] ? "cell live" : "cell dead";
+      rowsArr.push(
+        <Cell
+          cellClass={cellClass}
+          key={cellId}
+          cellId={cellId}
+          row={i}
+          col={j}
+          selectCell={props.selectCell}
+        />
+      );
     }
-    return (
-      <div className="grid" style={{ width: width }}>
-        {rowsArr}
-      </div>
-    );
   }
-}
+  return (
+    <div className="grid" style={{ width: width }}>
+      {rowsArr}
+    </div>
+  );
+};
 
 export default Grid;
